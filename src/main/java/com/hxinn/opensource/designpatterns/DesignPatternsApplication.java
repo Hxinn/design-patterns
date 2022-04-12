3bbebadf72fde53cf68c9e64e17652f6ebd1162e
xhx <xhx@estone.com>
2022-04-12 10:23:52 +0800

    start log

--- src/main/java/com/hxinn/opensource/designpatterns/DesignPatternsApplication.java
+++ src/main/java/com/hxinn/opensource/designpatterns/DesignPatternsApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DesignPatternsApplication {
 
     public static void main(String[] args) {
+        System.out.println("start----");
         SpringApplication.run(DesignPatternsApplication.class, args);
+        System.out.println("start success ====");
     }
 
 }
